3a4e7202062f89effefbf63b0188bbf013606fed
AlexandrVK <alexandrvk@gmail.com>
2023-05-10 11:08:10 +0300

    Добавлены переводы имени покемона

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -4,6 +4,8 @@ from django.db import models  # noqa F401
 class Pokemon(models.Model):
     title = models.CharField(max_length=200)
     photo = models.ImageField(upload_to='imgs', null=True)
+    title_en = models.CharField(max_length=200, null=True)
+    title_jp = models.CharField(max_length=200, null=True)
     description = models.TextField(null=True)
     def __str__(self):
         return f'{self.title}'
