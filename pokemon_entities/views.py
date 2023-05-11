8376cac452c446d6bdd768aa9e948c9bdde81057
AlexandrVK <alexandrvk@gmail.com>
2023-05-11 16:11:00 +0300

    Высушил код

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -68,7 +68,7 @@ def show_pokemon(request, pokemon_id):
             'img_url': request.build_absolute_uri(requested_pokemon.photo.url),
             }
    
-    next_evolution = requested_pokemon.pokemon_set.all().first()
+    next_evolution = requested_pokemon.parent_pokemon.first()
     if next_evolution:
         pokemon ['next_evolution'] = {
             'title_ru': next_evolution.title,
